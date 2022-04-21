4e141c06c324335afe4adab5524529834373bfcb
Louis Brunner <louis.brunner.fr@gmail.com>
2022-04-21 13:19:08 +0100

    feat: finish signed sessions (jwt style)

--- wurdle-server/src/session/manager.rs
+++ wurdle-server/src/session/manager.rs
@@ -1,5 +1,13 @@
 use super::session;
 use super::traits;
+use base64::{decode, encode};
+use flate2::write::{ZlibDecoder, ZlibEncoder};
+use flate2::Compression;
+use itertools::Itertools;
+use ring::{hmac, rand};
+use std::io::prelude::*;
+
+const SEPARATOR: &str = ".";
 
 #[derive(Clone)]
 pub struct SessionManager {
@@ -14,10 +22,44 @@ impl SessionManager {
     }
 
     pub fn serialize(&self, session: session::Session) -> Result<String, traits::Error> {
-        Ok(session.serialize()?)
+        let rng = rand::SystemRandom::new();
+        let key = hmac::Key::generate(hmac::HMAC_SHA256, &rng)?;
+
+        let serialized = session.serialize()?;
+        let compressed = {
+            let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+            encoder.write_all(serialized.as_bytes())?;
+            encoder.finish()?
+        };
+
+        let tag = hmac::sign(&key, compressed.as_ref());
+
+        let encoded = encode(compressed);
+        let encoded_tag = encode(tag.as_ref());
+
+        Ok(vec![encoded, encoded_tag].join(SEPARATOR))
     }
 
-    pub fn deserialize(&self, data: &str) -> Result<session::Session, traits::Error> {
-        Ok(session::Session::deserialize(data)?)
+    pub fn deserialize(&self, payload: &str) -> Result<session::Session, traits::Error> {
+        let rng = rand::SystemRandom::new();
+        let key = hmac::Key::generate(hmac::HMAC_SHA256, &rng)?;
+
+        let (encoded, encoded_tag) = payload
+            .splitn(2, SEPARATOR)
+            .collect_tuple()
+            .ok_or(traits::Error::InvalidFormatting)?;
+
+        let compressed = decode(encoded)?;
+        let tag = decode(encoded_tag)?;
+        hmac::verify(&key, compressed.as_ref(), tag.as_ref())?;
+
+        let serialized = {
+            let mut decoder = ZlibDecoder::new(Vec::new());
+            decoder.write_all(compressed.as_ref())?;
+            String::from_utf8(decoder.finish()?)?
+        };
+        let session = session::Session::deserialize(serialized.as_str())?;
+
+        Ok(session)
     }
 }
--- wurdle-server/src/session/traits.rs
+++ wurdle-server/src/session/traits.rs
@@ -4,4 +4,16 @@ use thiserror::Error;
 pub enum Error {
     #[error(transparent)]
     Json(#[from] serde_json::Error),
+    #[error(transparent)]
+    Compression(#[from] flate2::CompressError),
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    Base64(#[from] base64::DecodeError),
+    #[error(transparent)]
+    Utf8(#[from] std::string::FromUtf8Error),
+    #[error(transparent)]
+    Signing(#[from] ring::error::Unspecified),
+    #[error("invalid format")]
+    InvalidFormatting,
 }
